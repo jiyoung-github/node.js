0db0307be920ce0a0f308c04f0a822ad0c677f1b
Jiyoung lee <sto2120@naver.com>
2022-03-23 08:15:51 +0900

    zz

---
+++ lesson2.js
@@ -0,0 +1,6 @@
+
+console.log('output: %d',273); //[python] print('output: %d' % 273)
+console.log('%d + %d = %d', 273, 52, 273+52);
+
+console.log('%d +%d = %d', 273, 52, 273+52, 52273); // 특수문자와 매개변수의 개수 다를 경우엔?
+console.log('%d + %d = %d & %d', 273, 52, 273+52);
